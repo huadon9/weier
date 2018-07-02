2961c199868613c2bef11a5d1d91419661b3d83b
scinan <scinan@MacBook-Air-2.lan>
2018-07-02 14:37:08 +0800

    change home page style

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,28 +1,40 @@
-var swiperWidth = 0;
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    watchSlidesProgress: true,
+    initialSlide: 1,
+    autoplay: 3000,
+    // onProgress: function (swiper) {
+    //     var index = swiper.activeIndex;
+    //     for (var i = 0; i < swiper.slides.length; i++) {
+    //         var slide = swiper.slides[i];
+    //         var progress = slide.progress;
+    //         scale = 1 - Math.min(Math.abs(progress * 0.2), 1);
+
+    //         es = slide.style;
+    //         es.opacity = 1 - Math.min(Math.abs(progress / 2), 1);
+    //         es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = 'translate3d(0px,0,' + (-Math.abs(progress * 150)) + 'px)';
+    //     }
+    // },
+
+    onSetTransition: function (swiper, speed) {
+        for (var i = 0; i < swiper.slides.length; i++) {
+            es = swiper.slides[i].style;
+            es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = speed + 'ms';
+        }
+
+    }
+});
+
 $(document).ready(function () {
-    swiperWidth = $('.swiper-container').width();
-    var swiper = new Swiper('.swiper-container',{
-        loop: false,
-        autoplay: true,
-        initialSlide: 1,
-        // on: {
-        //     init: function () {
-        //         swiperConfig();
-        //     },
-        //     slideChangeTransitionEnd: function(){
-        //         swiperConfig();
-        //     },
-        // },
-    
+    var pulldown = pullUpLoad(function () {
+        //上拉的回调 在这里处理异步        
+        setTimeout(function () {
+            pulldown.notData(); //没有更多数据调用方法
+            // pulldown.resetload(); //重置 在异步成功之后并有新数据的时候调用
+        }, 2000)        
     });
 })
 
-
-function swiperConfig () {
-    $('.swiper-slide').removeAttr('style');
-    $('.swiper-slide').width(swiperWidth);
-    $('.swiper-slide-active').width(swiperWidth - 100);
-    $('.swiper-slide-active').css('margin', '0 50px');
-    $('.swiper-slide-prev').css('transform', 'translateX(35px)');
-    $('.swiper-slide-next').css('transform', 'translateX(-35px)');
-}
--- unpackage/assets/js/we.js
+++ unpackage/assets/js/we.js
@@ -19,3 +19,32 @@ $(document).ready(function () {
     })
 
 });
+
+//上拉加载更多
+function pullUpLoad (callback) {
+    var t;
+    $(window).scroll(function (e) {
+        var scTop = $(window).scrollTop();
+        var bodyHeight = $(document).height();
+        var windowHeight = $(window).height();
+        clearTimeout(t);
+        if (scTop + windowHeight >= bodyHeight) {
+            t = setTimeout(function () {
+                $('.pull-load').hide();
+                $('.pull-up-loading').show();
+                if (callback) {
+                    callback();
+                }
+            }, 500)
+        }
+    })
+    this.notData =  function () {
+        $('.pull-load').hide();
+        $('.pull-up-notdata').show();
+    }
+    this.resetload = function () {
+        $('.pull-load').hide();
+        $('.pull-up-title').show();
+    }
+    return this;
+}
