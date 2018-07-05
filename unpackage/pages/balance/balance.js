9e132543d90580cd7966cf7eb3f50707a7ede981
donghua <donghua@scinan.com>
2018-07-05 22:46:32 +0800

    add new page

---
+++ unpackage/pages/balance/balance.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.monthSelect').change(function (e) { 
+        var date = $(this).val();
+        var year = date.split('-')[0];
+        var month = date.split('-')[1];
+        $('.year-text').text(year);
+        $('.month-text').text(month);
+    })
+});
