6dc96d437983990809dd767b41a7edaa1f938266
donghua <donghua@scinan.com>
2018-07-25 09:54:20 +0800

    add new page

--- src/pages/applyRefund/applyRefund.js
+++ src/pages/applyRefund/applyRefund.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
-   $('.reason-text').keyup(function () {  
-       var text = $(this).val();
-       $('.text-count').text(140 - text.length)
-   })
+    $('.reason-text').keyup(function () {
+        var text = $(this).val();
+        $('.text-count').text(140 - text.length)
+    })
+    $('.sub').click(function () {
+        var number = parseInt($('.goods-number').val());
+        if (number < 2) {
+            $('.sub').addClass('disable');
+            $('.goods-number').val(0);
+            return;
+        }
+        $('.goods-number').val(--number);
+    })
+    $('.add').click(function () {
+        var number = parseInt($('.goods-number').val());
+        if (number > -1) {
+            $('.sub').removeClass('disable');
+        }
+        $('.goods-number').val(++number);
+    })
 });
