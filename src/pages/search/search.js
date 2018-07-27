20c0eca64fdb928454d0adcb75bef3ded5292948
donghua <donghua@scinan.com>
2018-07-27 10:43:07 +0800

    add new page and change style

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -6,4 +6,35 @@ $(document).ready(function () {
             // pulldown.resetload(); //重置 在异步成功之后并有新数据的时候调用
         }, 2000)        
     });
+
+    $('.show-type').click(function () {
+        if ($(this).hasClass('list')) {
+            $(this).removeClass('list').addClass('block');
+            $('.me-list').addClass('block-show');
+        } else {
+            $(this).removeClass('block').addClass('list');
+            $('.me-list').removeClass('block-show');
+        }
+    })
+
+    $('.choice-goods-btn').click(function () {
+        var h = $(window).height();
+        var ph = $('.poper').height();
+        $('.poper').css('bottom', h - 142 - ph + 'px');
+        $('.cover-bg').removeClass('hide');
+        $('.poper').removeClass('hide');
+    })
+
+    $('.cover-bg').click(function () {
+        hidePop();
+    })
+
+    $('.s-button').click(function () {
+        $('.hot-search').addClass('hide')
+    })
 })
+
+function hidePop () {
+    $('.cover-bg').addClass('hide');
+    $('.poper').css('bottom', '-100%');
+}
